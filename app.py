64511fd01a3677e5fc6670560b09dadd911e977d
Giorgio Braz <giorgiobraz@tutanota.com>
2021-11-20 10:28:03 -0300

    Opção de escolher livro físico ou eBook Fixes #4

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
+import os
 from urllib.request import urlopen
+# from urllib.error import URLError, HTTPError
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 # import sys
+
+load_dotenv()
+
+CAPA_DURA = os.getenv('CAPA_DURA')
+CAPA_COMUM = os.getenv('CAPA_COMUM')
+KINDLE_UNLIMITED = os.getenv('KINDLE_UNLIMITED')
+KINDLE = os.getenv('KINDLE')
+PRODUTO_FISICO = os.getenv('PRODUTO_FISICO')
+URL_ERROR = os.getenv('URL_ERROR')
+print(KINDLE_UNLIMITED)
 
 
 def url_to_html(url):
@@ -9,28 +22,28 @@ def url_to_html(url):
 
 
 def find_price_in_html(html):
-    content_html_page = BeautifulSoup(html, 'html.parser')
-    # price # id tag for physical books
-    # kindle-price # id tag for eBooks
-    # price_inside_buybox # id tag for physical products
+    # TODO PRODUTO_FISICO
+    # TODO KINDLE_UNLIMITED
+    # TODO PRODUTO/PREÇO NÃO ENCONTRADO
 
-    return content_html_page.find(id="price").get_text()
+    content_html_page = BeautifulSoup(html, 'html.parser')
+    selecao = content_html_page.find_all(
+        'span', class_="a-size-base a-color-price a-color-price")
+
+    return str(selecao)
 
 
 def get_formatted_price(price):
-    price_index = price.find('$') + 2
-    price = price[price_index:len(price)+1]
+    price_start = price.find('$') + 2
+    price_end = price.find(',') + 3
+    price = price[price_start:price_end]
     price = price.replace('.', '')
     price = price.replace(',', '.')
     return float(price)
 
 
-# url = 'https://www.amazon.com.br/1984-Exclusiva-Amazon-George-Orwell/dp/6586490162/ref=tmm_hrd_swatch_0?_encoding=UTF8&qid=1631567074&sr=1-3'
-
-url = 'https://www.amazon.com.br/Hit-Makers-Things-Become-Popular/dp/0241216028/ref=tmm_hrd_swatch_0?_encoding=UTF8&qid=&sr='
-
 # Main App
-html = url_to_html(url)
+html = url_to_html(CAPA_DURA)
 # html = url_to_html(sys.argv[1])
 price = find_price_in_html(html)
 price = get_formatted_price(price)
