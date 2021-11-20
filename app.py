2ec5732f25ba4e6f19594d9f0d94482173851d40
Giorgio Braz <giorgiobraz@tutanota.com>
2021-11-20 14:27:02 -0300

    Finding kindle unlimited and other product prices

--- app.py
+++ app.py
@@ -9,7 +9,9 @@ load_dotenv()
 
 CAPA_DURA = os.getenv('CAPA_DURA')
 CAPA_COMUM = os.getenv('CAPA_COMUM')
+LIVRO_DE_BOLSO = os.getenv('LIVRO_DE_BOLSO')
 KINDLE_UNLIMITED = os.getenv('KINDLE_UNLIMITED')
+KINDLE_UNLIMITED_2 = os.getenv('KINDLE_UNLIMITED_2')
 KINDLE = os.getenv('KINDLE')
 PRODUTO_FISICO = os.getenv('PRODUTO_FISICO')
 URL_ERROR = os.getenv('URL_ERROR')
@@ -21,9 +23,7 @@ def url_to_html(url):
     return response.read()
 
 
-def find_price_in_html(html):
-    # TODO PRODUTO_FISICO
-    # TODO KINDLE_UNLIMITED
+def get_book_price(html):
     # TODO PRODUTO/PREÇO NÃO ENCONTRADO
 
     content_html_page = BeautifulSoup(html, 'html.parser')
@@ -33,8 +33,26 @@ def find_price_in_html(html):
     return str(selecao)
 
 
+def get_kindle_unlimited_price(html):
+    content_html_page = BeautifulSoup(html, 'html.parser')
+    selecao = content_html_page.find_all(
+        'span', class_="extra-message olp-link")
+
+    return str(selecao)
+
+
+def get_product_price(html):
+    content_html_page = BeautifulSoup(html, 'html.parser')
+    selecao = content_html_page.find_all(
+        'span', class_="a-price a-text-price a-size-medium apexPriceToPay")
+
+    print(selecao)
+
+    return str(selecao)
+
+
 def get_formatted_price(price):
-    price_start = price.find('$') + 2
+    price_start = price.find('$') + 1
     price_end = price.find(',') + 3
     price = price[price_start:price_end]
     price = price.replace('.', '')
@@ -43,8 +61,10 @@ def get_formatted_price(price):
 
 
 # Main App
-html = url_to_html(CAPA_DURA)
+html = url_to_html(PRODUTO_FISICO)
 # html = url_to_html(sys.argv[1])
-price = find_price_in_html(html)
+# price = get_book_price(html)
+price = get_product_price(html)
+# price = get_kindle_unlimited_price(html)
 price = get_formatted_price(price)
 print(price)
