71e71c2bd457efb827763bf3925eb085ac134fa1
Giorgio Braz <giorgiobraz@tutanota.com>
2021-11-24 15:06:26 -0300

    Create a user flow

--- app.py
+++ app.py
@@ -4,51 +4,17 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 # import sys
-
 load_dotenv()
 
-CAPA_DURA = os.getenv('CAPA_DURA')
-CAPA_COMUM = os.getenv('CAPA_COMUM')
-LIVRO_DE_BOLSO = os.getenv('LIVRO_DE_BOLSO')
-KINDLE_UNLIMITED = os.getenv('KINDLE_UNLIMITED')
-KINDLE_UNLIMITED_2 = os.getenv('KINDLE_UNLIMITED_2')
+
+# TODO Send Push Notification
+
 KINDLE = os.getenv('KINDLE')
-PRODUTO_FISICO = os.getenv('PRODUTO_FISICO')
-URL_ERROR = os.getenv('URL_ERROR')
-print(KINDLE_UNLIMITED)
 
 
 def url_to_html(url):
     response = urlopen(url)
     return response.read()
-
-
-def get_book_price(html):
-    # TODO PRODUTO/PREÇO NÃO ENCONTRADO
-
-    content_html_page = BeautifulSoup(html, 'html.parser')
-    selecao = content_html_page.find_all(
-        'span', class_="a-size-base a-color-price a-color-price")
-
-    return str(selecao)
-
-
-def get_kindle_unlimited_price(html):
-    content_html_page = BeautifulSoup(html, 'html.parser')
-    selecao = content_html_page.find_all(
-        'span', class_="extra-message olp-link")
-
-    return str(selecao)
-
-
-def get_product_price(html):
-    content_html_page = BeautifulSoup(html, 'html.parser')
-    selecao = content_html_page.find_all(
-        'span', class_="a-price a-text-price a-size-medium apexPriceToPay")
-
-    print(selecao)
-
-    return str(selecao)
 
 
 def get_formatted_price(price):
@@ -60,11 +26,59 @@ def get_formatted_price(price):
     return float(price)
 
 
-# Main App
-html = url_to_html(PRODUTO_FISICO)
-# html = url_to_html(sys.argv[1])
-# price = get_book_price(html)
-price = get_product_price(html)
-# price = get_kindle_unlimited_price(html)
-price = get_formatted_price(price)
-print(price)
+def get_price(url):
+    html = url_to_html(url)
+    content_html_page = BeautifulSoup(html, 'html.parser')
+    book_price = content_html_page.find_all(
+        'span', class_="a-size-base a-color-price a-color-price")
+    kindle_unlimited_price = content_html_page.find_all(
+        'span', class_="extra-message olp-link")
+
+    if (book_price):
+        return get_formatted_price(str(book_price))
+
+    elif (kindle_unlimited_price):
+        return get_formatted_price(str(kindle_unlimited_price))
+
+    else:
+        return False
+
+
+def send_push_notification(url, preco_original, preco_ideal, push_option):
+    # preco_atual = get_price(url)
+    preco_atual = 0
+
+    if preco_atual <= preco_ideal:
+        print("Corre lá pra comprar!")
+        return
+
+    elif (preco_atual < preco_original) and (push_option == True):
+        print("O preço caiu")
+        return
+
+    else:
+        return False
+
+
+def main():
+    invalid_value = None
+
+    url = input("Cole abaixo a URL do livro físico ou eBook escolhido:")
+    preco_original = get_price(url)
+
+    if (type(preco_original) == float) or (type(get_price) == bool):
+        print("O preço atual do produto é:", preco_original)
+        while True:
+            preco_ideal = float(
+                input("Qual preço você quer pagar por esse produto?"))
+            if (preco_ideal != invalid_value) and (preco_ideal < preco_original):
+                break
+    else:
+        print("Não consegui encontrar o produto.")
+        return
+
+    # push_option = bool(input("Gostaria de receber...mesmo se..."))
+    # send_push_notification(url, preco_original, preco_ideal, push_option)
+
+
+main()
